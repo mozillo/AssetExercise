ad69c1d76e7563f704b000030f39880695e3b2f5
mot <446146366@qq.com>
2016-01-24 20:44:04 +0800

    add log increase or decrease

--- app/models/log.rb
+++ app/models/log.rb
@@ -7,4 +7,23 @@ class Log < ActiveRecord::Base
 	belongs_to :user, 
 						 :foreign_key => :user_uuid
 
+	after_destroy :restore_qty
+
+
+	def restore_qty
+		asset_manage = self.asset_manage
+		if self.action_type == 'in'
+			asset_manage.qty = asset_manage.qty.to_i - self.qty
+			
+			if asset_manage.qty.to_i < 0
+				asset_manage.qty = 0
+			end
+
+		elsif self.action_type == 'out'  
+			asset_manage.qty = asset_manage.qty.to_i + self.qty
+		end
+
+		asset_manage.save
+	end
+
 end
