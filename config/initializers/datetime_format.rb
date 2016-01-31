87d715057b07770c66bcd89c680cc1743c0d0eaa
mot <446146366@qq.com>
2016-01-31 23:45:27 +0800

    pg datetime format

--- config/initializers/datetime_format.rb
+++ config/initializers/datetime_format.rb
@@ -1,2 +1,2 @@
-Time::DATE_FORMATS[:default]= '%d-%m-%Y %H:%M:%S'
-Time::DATE_FORMATS[:db]= '%d-%m-%Y %H:%M:%S'
+Date::DATE_FORMATS[:default]= '%d-%m-%Y %H:%M:%S'
+Date::DATE_FORMATS[:db]= '%d-%m-%Y %H:%M:%S'
